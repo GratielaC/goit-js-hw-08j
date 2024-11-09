94da863c427add60105abfb874da51e14e2e6d3a
GratielaC <gratiela.chirtop@yahoo.com>
2024-11-09 21:17:02 +0200

    commit8

--- parcel-project-template/src/js/01-gallery.js
+++ parcel-project-template/src/js/01-gallery.js
@@ -1,28 +1,29 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-
-// Change code below this line
+// Import gallery items and SimpleLightbox library
+import { galleryItems } from './gallery-items.js';
 import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
 
+// Create gallery container and generate gallery markup
 const galleryContainer = document.querySelector('ul.gallery');
 const photosMarkup = createGalleryItem(galleryItems);
 
-function createGalleryItem(element) {
-  return element
+function createGalleryItem(items) {
+  return items
     .map(({ preview, original, description }) => {
       return `
             <a class="gallery__item" href="${original}">
-            <img class="gallery__image" src="${preview}" alt="${description}" />
+              <img class="gallery__image" src="${preview}" alt="${description}" />
             </a>`;
     })
     .join('');
 }
 
+// Insert generated markup into the gallery container
 galleryContainer.insertAdjacentHTML('beforeend', photosMarkup);
 
+// Initialize SimpleLightbox
 const galleryHandler = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionDelay: 250,
 });
+
 galleryHandler.on('show.simplelightbox');
